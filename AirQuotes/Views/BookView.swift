7b4da040c47de0a33bd5f0a0b60463130f515efc
Carmine Porricelli <carmineporricelli96@gmail.com>
2021-11-15 21:19:05 +0100

    BookView Style changes

--- AirQuotes/Views/BookView.swift
+++ AirQuotes/Views/BookView.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 struct BookView: View {
+    let color: Color
+    let text: String
+    
     var body: some View {
         Rectangle()
-            .fill(Color.bookColorOne)
+            .fill(color)
             .cornerRadius(10)
             .aspectRatio(0.625, contentMode: .fit)
             .shadow(color: shadowColor, radius: 15, x: 0, y: 0)
             .overlay {
                 VStack {
-                    Text("Harry Potter And Thedsadsad sadsadsadsad sadsadsadsadsadds sdewjn")
-                        .font(.largeTitle)
-                        .fontWeight(.black)
-                        .padding()
+                    Text(text)
+                        .font(.system(size: 60, weight: .heavy))
+                        .foregroundColor(.secondary)
                         .lineLimit(4)
+                        .padding()
                         
                     Spacer()
                 }
@@ -35,7 +38,25 @@ struct BookView: View {
 
 struct BookView_Previews: PreviewProvider {
     static var previews: some View {
-        BookView()
+        BookView(color: .bookColorOne, text: "Harry Potter and the Test")
             .padding()
+    }
+}
+
+// Carmine: Ho provato a mettere un'ombreggiatura interna nel testo del libro per simulare un testo inciso.. Non mi ha convinto.
+// https://www.youtube.com/watch?v=ndDmoXbNZvA
+private extension Text {
+    func innerShadow<V: View>(_ background: V, radius: CGFloat = 5, opacity: Double = 0.7) -> some View {
+        self
+            .foregroundColor(.clear)
+            .overlay(background.mask(self))
+            .overlay(
+                ZStack {
+                    self.foregroundColor(Color(white: 1 - opacity))
+                    self.foregroundColor(.white).blur(radius: radius)
+                }
+                .mask(self)
+                .blendMode(.multiply)
+            )
     }
 }
