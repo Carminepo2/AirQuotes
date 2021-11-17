e13a16f031ad252b0b9ff4a11a46a0169c5927d6
Carmine Porricelli <carmineporricelli96@gmail.com>
2021-11-18 00:03:55 +0100

    Aggiunto componente ImagePicker

---
+++ AirQuotes/Utilities/ImagePicker.swift
@@ -0,0 +1,61 @@
+//
+//  ImagePicker.swift
+//  AirQuotes
+//
+//  Created by Carmine Porricelli on 17/11/21.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+
+// In SwiftUI NON esiste ancora un modo "ufficiale" per richiamare la view della camera/image picker.
+// Tutto ciò che c'è qui serve a portare la classe UIKit UIImagePicker a SwiftUI
+// Risorsa utilizzata: https://www.youtube.com/watch?v=Y-65T0YBOm4
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    typealias Coordinator = ImagePickerCoordinator
+    
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    
+    var sourceType: UIViewControllerType.SourceType = .camera
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = sourceType
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> ImagePicker.Coordinator {
+        return ImagePickerCoordinator(image: $image, isShown: $isShown)
+    }
+}
+
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @Binding var image: UIImage?
+    @Binding var isShown: Bool
+    
+    init(image: Binding<UIImage?>, isShown: Binding<Bool>) {
+        _image = image
+        _isShown = isShown
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let uiImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            image = uiImage
+            isShown = false
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShown = false
+    }
+}
