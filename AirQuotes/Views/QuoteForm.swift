c7dcd027839acf2d140f138abb91739d22610c32
valeriooconte <valeriodomenicoconte@gmail.com>
2021-11-18 16:16:41 +0100

    Added QuoteForm

---
+++ AirQuotes/Views/QuoteForm.swift
@@ -0,0 +1,40 @@
+//
+//  QuoteForm.swift
+//  AirQuotes
+//
+//  Created by Valerio Domenico Conte on 18/11/21.
+//
+
+import SwiftUI
+import ImageCaptureCore
+
+
+struct QuoteForm: View {
+    @State private var quote = ""
+    @State private var by = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Quote", text: $quote)
+                    .frame(height: 150)
+                TextField("By", text: $by)
+                    .frame(height: 50)
+            }
+            .navigationTitle("Add a new quote")
+            .toolbar {
+                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                    Text("Save")        //Button
+                    
+                    
+                }
+            }
+        }
+    }
+}
+
+struct QuoteForm_Previews: PreviewProvider {
+    static var previews: some View {
+        QuoteForm()
+    }
+}
