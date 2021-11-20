ddb86f34cd9f0bbfb6df22bb1ed8d0a25609ff26
Carmine Porricelli <carmineporricelli96@gmail.com>
2021-11-20 12:49:19 +0100

    Restyling create book view

--- AirQuotes/Views/CreationBookView.swift
+++ AirQuotes/Views/CreationBookView.swift
@@ -9,78 +9,89 @@ import SwiftUI
 
 struct BookCreationView: View {
     
-    
-    var book: BookModel
-    
     @State var chosenColor: String = "BookGreen"
     @Binding var isCreateBookModalOpen : Bool
     @State private var author = ""
     @State private var title = ""
     @Environment(\.managedObjectContext) var managedObjectContext
     
-    
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                BookView(color: Color(chosenColor), text: title)
-                    .frame(height: 200)
-                
-                Form {
-                    TextField("Title", text: $title)
-                        .frame(height: 50)
-                    TextField("Author", text: $author)
-                        .frame(height: 50)
-                }
-                .padding(.vertical)
-                
-                HStack{
-                    Text("Choose a Color")
-                        .font(.title2)
-                        .foregroundColor(.black)
-                        .opacity(0.8)
+        
+        
+        ZStack {
+            Color.themeColor
+                .ignoresSafeArea()
+            
+            NavigationView {
+                VStack {
+                    BookView(color: Color(chosenColor), text: title)
+                        .frame(height: 220)
+                    
+                    Form {
+                        TextField("Title", text: $title)
+                            .frame(height: 50)
+                        TextField("Author", text: $author)
+                            .frame(height: 50)
+                        
+                        HStack{
+                            ColorButtonPicker(colorName: "BookRed", chosenColor: $chosenColor)
+                            ColorButtonPicker(colorName: "BookGreen", chosenColor: $chosenColor)
+                            ColorButtonPicker(colorName: "BookPink", chosenColor: $chosenColor)
+                            ColorButtonPicker(colorName: "BookLightBlue", chosenColor: $chosenColor)
+                        }
                         .padding(.horizontal)
-                    Spacer()
+                        .padding(.top)
+
+                    }
+                    .onAppear {
+                        UITableView.appearance().backgroundColor = .clear
+                    }
+                    
+                    
                 }
-                
-                HStack{
-                    ColorButton(color: "BookRed", chosenColor: $chosenColor)
-                    ColorButton(color: "BookGreen", chosenColor: $chosenColor)
-                    ColorButton(color: "BookPink", chosenColor: $chosenColor)
-                    ColorButton(color: "BookLightBlue", chosenColor: $chosenColor)
-                }
-                .padding()
-            }
-            .toolbar(content: {
-                Button("Add", action: {
-                    PersistenceController.shared.createBook(color: chosenColor, title: title, author: author)
-                    isCreateBookModalOpen = false
+                .offset(y: -20)
+                .toolbar(content: {
+                    Button("Add", action: {
+                        PersistenceController.shared.createBook(color: chosenColor, title: title, author: author)
+                        isCreateBookModalOpen = false
+                    })
                 })
-            })
+            }
+            
+            
+            
+            
         }
     }
 }
 
+
 struct BookCreationView_Previews: PreviewProvider {
     static var previews: some View {
-        BookCreationView(book: .example, isCreateBookModalOpen: .constant(true))
+        BookCreationView(isCreateBookModalOpen: .constant(true))
     }
 }
 
-struct ColorButton: View {
+struct ColorButtonPicker: View {
     
-    let color: String
+    let colorName: String
     @Binding var chosenColor: String
     
     var body: some View {
-        Button {
-            chosenColor = color
-        } label: {
+        ZStack {
             Circle()
-                .frame(width: 50, height: 50)
-                .foregroundColor(Color(color))
-                .padding(.horizontal, 10)
-                .overlay(Circle().stroke(Color.black, lineWidth: 0.5))
+                .fill(Color(colorName))
+                .frame(width: 40, height: 40)
+                .onTapGesture(perform: {
+                    chosenColor = colorName
+                })
+                .padding(10)
+            
+            if chosenColor == colorName {
+                Circle()
+                    .stroke(Color(colorName), lineWidth: 5)
+                    .frame(width: 50, height: 50)
+            }
         }
     }
 }
--- AirQuotes/Views/HomeView/HomeView.swift
+++ AirQuotes/Views/HomeView/HomeView.swift
@@ -72,7 +72,7 @@ struct HomeView: View {
                             isCreateBookModalOpen.toggle()
                          }
                         .sheet(isPresented: $isCreateBookModalOpen, content: {
-                            BookCreationView(book: .example, isCreateBookModalOpen: $isCreateBookModalOpen)
+                            BookCreationView(isCreateBookModalOpen: $isCreateBookModalOpen)
                         })
                     
                     }.padding(.horizontal)
